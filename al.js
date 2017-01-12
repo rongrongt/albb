d86841e744b5cb6cda8cdfeff574074d638555b5
rongrongt <dy@rrtekiMacBook-Air.local>
2017-01-13 00:16:02 +0800

    first

---
+++ al.js
@@ -0,0 +1,196 @@
+	var datas = [
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '999.00',
+			oldPrice: '799.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '4449.00',
+			oldPrice: '99.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '999.00',
+			oldPrice: '799.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '4449.00',
+			oldPrice: '99.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		}
+
+	];
+
+
+	var huges = [
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '999.00',
+			oldPrice: '799.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '4449.00',
+			oldPrice: '99.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},	
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '159.00',
+			oldPrice: '299.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '999.00',
+			oldPrice: '799.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		},
+
+		{
+			imgsrc: './alimg/xxx.jpg',
+			title: 'New design smart-phone',
+			price: '4449.00',
+			oldPrice: '99.00',
+			moq: '100',
+			buyurl: 'www.baidu.com',
+
+		}
+
+
+	];
+
+	
+	$('#nt-hd-products').html( _.template($('#nt-hd-template').html(), datas));
+	$('#nt-huge-products').html( _.template($('#nt-huge-template').html(), huges));
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+				
+
+
+
+
+
+
+
+
+
+
+				
